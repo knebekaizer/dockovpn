90b7b9b49caff89a81462e7832a65134a7cf4a1a
Vladimir Ivanov <knebekaizer@gmail.com>
2023-03-08 23:18:59 +0300

    Create local client.ovpn
    Do not expose http server
    Run `docker exec dockvpn ./genclient.sh foo` to generate client "foo"

--- scripts/genclient.sh
+++ scripts/genclient.sh
@@ -2,7 +2,7 @@
 
 source ./functions.sh
 
-CLIENT_PATH="$(createConfig)"
+CLIENT_PATH="$(createConfig $@)"
 CONTENT_TYPE=application/text
 FILE_NAME=client.ovpn
 FILE_PATH="$CLIENT_PATH/$FILE_NAME"
@@ -69,9 +69,9 @@ then
 fi
 echo "$(datef) $FILE_PATH file has been generated"
 
-echo "$(datef) Config server started, download your $FILE_NAME config at http://$HOST_ADDR:$HOST_CONF_PORT/"
-echo "$(datef) NOTE: After you download your client config, http server will be shut down!"
-
-{ echo -ne "HTTP/1.1 200 OK\r\nContent-Length: $(wc -c <$FILE_PATH)\r\nContent-Type: $CONTENT_TYPE\r\nContent-Disposition: attachment; fileName=\"$FILE_NAME\"\r\nAccept-Ranges: bytes\r\n\r\n"; cat "$FILE_PATH"; } | nc -w0 -l 8080
-
-echo "$(datef) Config http server has been shut down"
+# echo "$(datef) Config server started, download your $FILE_NAME config at http://$HOST_ADDR:$HOST_CONF_PORT/"
+# echo "$(datef) NOTE: After you download your client config, http server will be shut down!"
+#
+# { echo -ne "HTTP/1.1 200 OK\r\nContent-Length: $(wc -c <$FILE_PATH)\r\nContent-Type: $CONTENT_TYPE\r\nContent-Disposition: attachment; fileName=\"$FILE_NAME\"\r\nAccept-Ranges: bytes\r\n\r\n"; cat "$FILE_PATH"; } | nc -w0 -l 8080
+#
+# echo "$(datef) Config http server has been shut down"
